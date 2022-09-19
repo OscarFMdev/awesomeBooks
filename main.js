74282689dc3c0dcdc45a860b372bb93feb598ffa
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-09-19 14:37:19 -0500

    Add selectors

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ function book(name, author) {
 /* Create booksArray and set it to empty if localStorage is null */
 const booksArray = JSON.parse(localStorage.getItem('books')) || [];
 
+/* Selector */
+const bookList = document.querySelector('.book-list');
+const bookName = document.querySelector('.book-name');
+const author = document.querySelector('.author');
+const form = document.querySelector('form');
+
