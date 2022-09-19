20ccb916a55c18f7a475aae015d78e5271f67f8b
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-09-19 15:13:40 -0500

    Fix linter errors

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-/*Object constructor*/
-function book(name, author) {
+/* Object constructor */
+function Book(name, author) {
   this.name = name;
   this.author = author;
 }
@@ -14,38 +14,36 @@ const author = document.querySelector('.author');
 const form = document.querySelector('form');
 
 /* Functions */
-function addBook(e) {
-  e.preventDefault();
-  const bookItem = new book(bookName.value, author.value);
-  booksArray.push(bookItem);
-  localStorage.setItem('books', JSON.stringify(booksArray));
-  displayBooks();
-}
-
 function displayBooks() {
   const books = JSON.parse(localStorage.getItem('books'));
-  bookList.innerHTML = books.map((book, i) => {
-    return `
+  bookList.innerHTML = books.map((book, i) => `
     <li class="book">
       <h2 class="book-title">${book.name}</h2>
       <p class="book-author">${book.author}</p>
       <button class="remove-book" data-index=${i}>Remove</button>
     </li>
     <hr style="width: 500px; margin-left: 0;">
-    `;
-  }).join('');
+    `).join('');
+}
+
+function addBook(e) {
+  e.preventDefault();
+  const bookItem = new Book(bookName.value, author.value);
+  booksArray.push(bookItem);
+  localStorage.setItem('books', JSON.stringify(booksArray));
+  displayBooks();
 }
 
 function removeBook(e) {
   if (!e.target.matches('button')) return;
-  const index = e.target.dataset.index;
+  const { index } = e.target.dataset;
   booksArray.splice(index, 1);
   localStorage.setItem('books', JSON.stringify(booksArray));
   displayBooks();
 }
 
 /* Event Listeners */
-  
+
 form.addEventListener('submit', addBook);
 
 bookList.addEventListener('click', removeBook);
