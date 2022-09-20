31950c01472cc19a015444419846c2b676921c64
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-09-20 14:05:53 -0500

    Add JS class

--- main.js
+++ main.js
@@ -1,49 +1,53 @@
-/* Object constructor */
-function Book(name, author) {
-  this.name = name;
-  this.author = author;
-}
-
-/* Create booksArray and set it to empty if localStorage is null */
-const booksArray = JSON.parse(localStorage.getItem('books')) || [];
-
 /* Selector */
 const bookList = document.querySelector('.book-list');
 const bookName = document.querySelector('.book-name');
 const author = document.querySelector('.author');
 const form = document.querySelector('form');
 
-/* Functions */
-function displayBooks() {
-  bookList.innerHTML = booksArray.map((book, i) => `
-    <li class="book">
-      <h2 class="book-title">${book.name}</h2>
-      <p class="book-author">${book.author}</p>
-      <button class="remove-book" data-index=${i}>Remove</button>
-    </li>
-    <hr style="width: 500px; margin-left: 0;">
-    `).join('');
+/* Object constructor */
+function Book(name, author) {
+  this.name = name;
+  this.author = author;
 }
 
-function addBook(e) {
-  e.preventDefault();
-  const bookItem = new Book(bookName.value, author.value);
-  booksArray.push(bookItem);
-  localStorage.setItem('books', JSON.stringify(booksArray));
-  displayBooks();
+class Collection {
+  constructor() {
+    this.booksArray = JSON.parse(localStorage.getItem('books')) || [];
+  }
+
+  displayBooks() {
+    bookList.innerHTML = this.booksArray.map((book, i) => `
+      <li class="book">
+        <h2 class="book-title">${book.name}</h2>
+        <p class="book-author">${book.author}</p>
+        <button class="remove-book" data-index=${i}>Remove</button>
+      </li>
+      <hr style="width: 500px; margin-left: 0;">
+      `).join('');
+  }
+
+  addBook(e) {
+    e.preventDefault();
+    const bookItem = new Book(bookName.value, author.value);
+    this.booksArray.push(bookItem);
+    localStorage.setItem('books', JSON.stringify(this.booksArray));
+    this.displayBooks();
+  }
+
+  removeBook(e) {
+    if (!e.target.matches('button')) return;
+    const { index } = e.target.dataset;
+    this.booksArray.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(this.booksArray));
+    this.displayBooks();
+  }
 }
 
-function removeBook(e) {
-  if (!e.target.matches('button')) return;
-  const { index } = e.target.dataset;
-  booksArray.splice(index, 1);
-  localStorage.setItem('books', JSON.stringify(booksArray));
-  displayBooks();
-}
+const collection = new Collection();
 
-/* Event Listeners */
+/* Displays books on DOM loaded */
+collection.displayBooks();
 
-form.addEventListener('submit', addBook);
-bookList.addEventListener('click', removeBook);
-
-window.addEventListener('DOMContentLoaded', displayBooks);
+/*  */
+form.addEventListener('submit', collection.addBook.bind(collection));
+bookList.addEventListener('click', collection.removeBook.bind(collection));
